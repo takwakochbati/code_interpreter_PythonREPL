df21378a3e13986abd9d7c2f6ba213d07651db8f
Takwa Kochbati <kochbatitakwa1@gmail.com>
2024-11-13 17:44:56 +0100

    create a router agent to send instruction to both CSV & Python Agents

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
-from langchain_community.document_loaders import ReadTheDocsLoader
+from typing import Any
 from dotenv import load_dotenv
 from langchain.agents import create_react_agent, AgentExecutor, AgentType
 from langchain import hub
-from langchain_experimental.agents import create_csv_agent, create_pandas_dataframe_agent
+from langchain_core.tools import Tool
+from langchain_experimental.agents import (
+    create_csv_agent,
+    create_pandas_dataframe_agent,
+)
 from langchain_experimental.tools import PythonREPLTool
 from langchain_ollama import ChatOllama
 import pandas as pd
-
+from numpy.f2py.crackfortran import verbose
 
 load_dotenv()
 
@@ -29,24 +33,69 @@ def main():
         llm=ChatOllama(model="llama3.1", temperature=0),
         tools=tools,
     )
-    agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True, handle_parsing_errors=True)
-
-    agent_executor.invoke(
-        input={
-            "input": """generate a QRcode that points to https://github.com/takwakochbati, you have qrcode package installed already
-                        and then create a folder in the current working directory and save the generated qrcode in it """
-        }
+    python_agent_executor = AgentExecutor(
+        agent=agent, tools=tools, verbose=True, handle_parsing_errors=True
     )
 
-    csv_agent = create_csv_agent(
+    # python_agent_executor.invoke(
+    #    input={
+    #        "input": """generate a QRcode that points to https://github.com/takwakochbati, you have qrcode package installed already
+    #                    and then create a folder in the current working directory and save the generated qrcode in it """
+    #    }
+    # )
+
+    csv_agent_executor: AgentExecutor = create_csv_agent(
         llm=ChatOllama(model="llama3.1", temperature=0),
         path="episode_info.csv",
-        verbose = True,
+        verbose=True,
         agent_type=AgentType.ZERO_SHOT_REACT_DESCRIPTION,
     )
 
-    csv_agent.invoke(input={"input" : "how many columns are there in file episode_info.csv"})
-    csv_agent.run("how many episodes are in season 4?")
+    # csv_agent_executor.invoke(input={"input" : "how many columns are there in file episode_info.csv"})
+    # csv_agent_executor.run("how many episodes are in season 4?")
+
+    ############### *Create a router Agent that sends the work to either Python Agent or CSV Agent* ##############################
+    def python_agent_executor_wrapper(original_prompt: str) -> dict[str, Any]:
+        return python_agent_executor.invoke({"input": original_prompt})
+
+    tools = [
+        Tool(
+            name="Python Agent",
+            func=python_agent_executor.invoke,
+            description="""useful when you need to transform natural language to python and execute the python code,
+                          returning the results of the code execution
+                          DOES NOT ACCEPT CODE AS INPUT""",
+        ),
+        Tool(
+            name="CSV Agent",
+            func=csv_agent_executor.invoke,
+            description="""useful when you need to answer question over episode_info.csv file,
+                         takes an input the entire question and returns the answer after running pandas calculations""",
+        ),
+    ]
+    prompt2 = base_prompt.partial(instructions="")
+    router_agent = create_react_agent(
+        prompt=prompt2,
+        llm=ChatOllama(model="llama3.1", temperature=0),
+        tools=tools,
+    )
+    router_agent_executor = AgentExecutor(router_agent, tools=tools, verbose=True)
+
+    print(
+        router_agent_executor.invoke(
+            {
+                "input": " which season has the most episodes?",
+            }
+        )
+    )
+    print(
+        router_agent_executor.invoke(
+            {
+                "input": "Generate and save in current working directory 15 qrcodes that point to `www.udemy.com/course/langchain`",
+            }
+        )
+    )
+
 
 if __name__ == "__main__":
     main()
