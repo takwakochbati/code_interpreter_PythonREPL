2e5e5ced2ffc87a05d118012592ea2116cf45353
Takwa Kochbati <kochbatitakwa1@gmail.com>
2024-11-14 10:01:02 +0100

    tool calling with agents & Tavily

---
+++ tool_agent_calling.py
@@ -0,0 +1,41 @@
+from dotenv import load_dotenv
+from langchain_ollama import ChatOllama
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.tools import tool
+from langchain.agents import create_tool_calling_agent, AgentExecutor
+from langchain_community.tools.tavily_search import TavilySearchResults
+
+load_dotenv()
+
+
+@tool
+def multiply(x: float, y: float) -> float:
+    """Multiply 'x' times 'y'."""
+    return x * y
+
+
+if __name__ == "__main__":
+    print("Hello Tool Calling")
+
+    prompt = ChatPromptTemplate.from_messages(
+        [
+            ("system", "you're a helpful assistant"),
+            ("human", "{input}"),
+            ("placeholder", "{agent_scratchpad}"),
+        ]
+    )
+
+    tools = [TavilySearchResults(), multiply]
+    # llm = ChatOpenAI(model="gpt-4-turbo")
+    llm = ChatOllama(model="llama3.1", temperature=0)
+
+    agent = create_tool_calling_agent(llm, tools, prompt)
+    agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
+
+    res = agent_executor.invoke(
+        {
+            "input": "what is the weather in dubai right now? compare it with San Fransisco, output should in in celsious",
+        }
+    )
+
+    print(res)
